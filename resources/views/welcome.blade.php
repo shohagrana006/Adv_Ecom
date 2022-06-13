32ab545e92820cd47bf7b7b20ee873a994bd1cf8
shohagrana006 <shohagrana006@gmail.com>
2022-06-14 03:06:23 +0600

    webpack Mix file and its assets generate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{config('app.name')}}</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{mix('css/all.css')}}">
+  
+        <link rel="stylesheet" href="{{mix('css/app.css')}}">
+        <link rel="stylesheet" href="{{mix('css/style.css')}}">
     </head>
     <body class="antialiased">
     <header>
--- webpack.mix.js
+++ webpack.mix.js
@@ -5,17 +5,17 @@ const mix = require('laravel-mix');
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | Mix provides a clean, fluent API for defining some Webpack build | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js([
-        'resources/js/app.js',
-        'resources/js/custom.js',
-    ], 'public/js/all.js')
+mix.sass('resources/sass/app.scss','public/css')
     .styles([
-        'resources/css/app.css',
         'resources/css/style.css'
-    ], 'public/css/all.css').version();
+    ], 'public/css/style.css')
+    .js([
+        'resources/js/app.js',
+        'resources/js/custom.js'
+    ], 'public/js/all.js')
+    .sourceMaps().version();
