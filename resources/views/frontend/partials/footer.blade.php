9ae281ada4dc94d1a1342aa1da6de2867ff24dfc
shohagrana006 <shohagrana006@gmail.com>
2022-06-14 20:23:42 +0600

    footer and header customize

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -3,7 +3,7 @@
       <p class="float-end mb-1">
         <a href="#">Back to top</a>
       </p>
-      <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-      <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/docs/5.2/getting-started/introduction/">getting started guide</a>.</p>
+      <p class="mb-1"> &copy; By Adv Ecom {{date('Y')}}</p>
+      <p class="mb-0"><a href="#">contact us</a></p>
     </div>
   </footer>
--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -13,9 +13,16 @@
           <div class="col-sm-4 offset-md-1 py-4">
             <h4 class="text-white">Contact</h4>
             <ul class="list-unstyled">
-              <li><a href="#" class="text-white">Follow on Twitter</a></li>
-              <li><a href="#" class="text-white">Like on Facebook</a></li>
+              @guest
+              <li><a href="#" class="text-white">Login</a></li>
+              <li><a href="#" class="text-white">Sign Up</a></li>
               <li><a href="#" class="text-white">Email me</a></li>
+              @endguest
+              @auth
+              <li><a href="#" class="text-white">Follow on Twitter</a></li>
+              <li><a href="#" class="text-white">Like on Facebook</a></li>             
+              @endauth
+              
             </ul>
           </div>
         </div>
