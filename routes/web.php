de151d05867754bdd7b21524bae41b55d23d8fbd
shohagrana006 <shohagrana006@gmail.com>
2022-06-15 21:30:35 +0600

    first route add

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\{
+    FrontendController
+};
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('frontend.layout.home');
+// Route::group(['namespace'=>'App\Http\Controllers\Frontend'], function(){
+//     Route::get('/','FrontendController@index')->name('index');
+// });
+
+Route::group(['name'=>'frontend.'], function(){
+    Route::get('/',[FrontendController::class, 'index'])->name('index');
 });
+
