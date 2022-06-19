da61926bfdfe9695c3c5f90185f780b9f27e2096
shohagrana006 <shohagrana006@gmail.com>
2022-06-19 18:40:36 +0600

    product controller add

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function productDetails($slug){
+        
         $data = [];
         $data['product'] = Product::where('slug', $slug)->where('active',1)->first();
 
